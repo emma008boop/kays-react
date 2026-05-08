cba4a3a813f8d451f8360130d85a02a4d5a5d3a2
Emma <zuluagamoraemmanuel@gmail.com>
2026-05-07 21:22:32 -0500

    Feature: Logo component added

---
+++ src/components/ui/logo/Logo.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import logoImg from '../../../assets/img/brand/no_background.png';
+
+const Logo = () => {
+    return (
+        <Link to="/" className="brand-logo">
+            <img src={logoImg} alt="Nombre de tu Marca" />
+        </Link>
+    );
+}
+
+export default Logo;
